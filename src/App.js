84f1cd40bd6723e4b803f2488a4eb31b5a15f136
Susan Ye <susanye3@gmail.com>
2020-09-10 23:53:54 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  constructor (props) {
+    super(props) 
+    
+  } 
+  render(){
+    return (
+      <div className="portfolio">
+        {/* <NavBar/>
+        <SectionContainer /> */}
+      </div>
+    );
+  }   
+};
+
+export default App;
